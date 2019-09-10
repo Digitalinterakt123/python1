4b342b6d59620bb299d21828192716a7f8c5ee55
praneeth <praneeth@digitalinterakt.com>
2019-09-10 12:00:48 +0530

    date_functions code changed and commit

--- practice/Date_functions.py
+++ practice/Date_functions.py
@@ -1,10 +1,10 @@
 import time,calendar;
 
-localtime = time.localtime(time.time())
+# localtime = time.localtime(time.time())
 localtime1 = time.asctime( time.localtime(time.time()) )
-localtime12 = time.clock()
-print("Local current time :", localtime12)
+# localtime12 = time.clock()
+print("Local current time :", localtime1)
 
-# cal = calendar.month(2008, 8)
-# print("Here is the calendar:")
-# print (cal)
+cal = calendar.month(2008, 9)
+print("Here is the calendar:")
+print(cal)
